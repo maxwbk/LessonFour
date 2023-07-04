4c5c0ad37ba42ef41655d1862da9c43a038ab310
maxwbk <maxwbk@yandex.ru>
2023-07-04 23:49:35 +0700

    task1 ready

---
+++ task1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+
+}
+
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power += powerBase;
+    }
+    return power;
+}
+
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int powerBase = Prompt("ВВедите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValidateExponent (exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power (powerBase, exponent)}");
+}
